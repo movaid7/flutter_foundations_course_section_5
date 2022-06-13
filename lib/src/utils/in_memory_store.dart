8cce83307f772bed122f2db6a8b9f7a329a126bf
Muhammad Vaid <molvy32@gmail.com>
2022-06-13 14:31:39 +0200

    Added docs for InMemoryStore/BehaviorSubject

--- lib/src/utils/in_memory_store.dart
+++ lib/src/utils/in_memory_store.dart
@@ -1,12 +1,23 @@
 import 'package:rxdart/rxdart.dart';
 
+// An in-memory store backed by BehaviorSubject that can be
+// used to store data for all fake repositories in the app.
+// This class is a wrapper over BehaviorSubject
 class InMemoryStore<T> {
   InMemoryStore(T initial) : _subject = BehaviorSubject<T>.seeded(initial);
+
+  // The BehaviorSubject that holds the data
   final BehaviorSubject<T> _subject;
 
-  Stream<T> get stream => _subject.stream; // Get the stream of data
-  T get value => _subject.value; // Get latest value from stream
-  set value(T newValue) => _subject.add(newValue); // Add new value to stream
+  // The output stream that can be used to listen to the data
+  Stream<T> get stream => _subject.stream;
 
-  void close() => _subject.close(); // Close the stream
+  // A synchronous getter for the current value
+  T get value => _subject.value;
+
+  // A setter for updating the value
+  set value(T newValue) => _subject.add(newValue);
+
+  // Call this when done
+  void close() => _subject.close();
 }
