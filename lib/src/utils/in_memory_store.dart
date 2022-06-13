a199f45fed4e629dddf8e5fc366caace3cbae621
Muhammad Vaid <molvy32@gmail.com>
2022-06-13 14:28:02 +0200

    Added RxDart InMemoryStore generic class

--- lib/src/utils/in_memory_store.dart
+++ lib/src/utils/in_memory_store.dart
@@ -1,7 +1,12 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:rxdart/rxdart.dart';
 
 class InMemoryStore<T> {
   InMemoryStore(T initial) : _subject = BehaviorSubject<T>.seeded(initial);
   final BehaviorSubject<T> _subject;
+
+  Stream<T> get stream => _subject.stream; // Get the stream of data
+  T get value => _subject.value; // Get latest value from stream
+  set value(T newValue) => _subject.add(newValue); // Add new value to stream
+
+  void close() => _subject.close(); // Close the stream
 }
